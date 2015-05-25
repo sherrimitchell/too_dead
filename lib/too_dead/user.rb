bf0e83a4d71ee7b14beb84759de6386e513287f8
Brit Butler <redline6561@gmail.com>
2015-05-25 11:24:24 -0400

    Add first pass at associations.

--- lib/too_dead/user.rb
+++ lib/too_dead/user.rb
@@ -1,4 +1,8 @@
 module TooDead
   class User < ActiveRecord::Base
+    has_many :todo_lists
+    ## The following association is reasonable but only necessary
+    ## if we want to see or query all todo_items for a given user at once.
+    # has_many :todo_items, through: :todo_lists
   end
 end
